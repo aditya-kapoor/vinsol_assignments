3c3e5601b8f8bfc33eb87457edc0fecfb6bacb61
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-07-27 18:42:14 +0530

    Corrected Code JQuery Ex-1

--- jquery-excercise-1/js/sandbox.js
+++ jquery-excercise-1/js/sandbox.js
@@ -6,18 +6,18 @@ $(document).ready(function(){
     /* Excercise 2.1.2 */
     /*"Come up with three selectors that you could use to get the third item in the #myList unordered list. Which is the best to use? Why?"*/
     $("ul#myList").children().eq(2)
-    $("div#main #myList").children().eq(2))
+    $("div#main #myList").children().eq(2)
     $("#myList").children().eq(2)
     /*"Out of the three methods given above, the last method is slowest while the first one is fastest"*/
     
     /*"Excercise 2.1.3"*/
     /*"Select the label for the search input using an attribute selector"*/
-    $('label[for="q"]'))
+    $('label[for="q"]')
 
    /*"Excercise 2.1.4"*/
    /*"Figure out how many elements on the page are hidden"*/
-   $(document).find("body").find(":hidden").length) // Hidden inside body
-   $(document).find(":hidden").length)  //hidden throughout page
+   $(document).find("body").find(":hidden").length // Hidden inside body
+   $(document).find(":hidden").length  //hidden throughout page
 
 
    /*"Excercise 2.1.5"*/
@@ -29,7 +29,7 @@ $(document).ready(function(){
  
    /*"Excercise 2.1.6"*/
    /*"Select all of the odd table rows in the table body."*/
-   $('table').find('tr').filter(":odd").length)
+   $('table').find('tr').filter(":odd")
    
 
 }); 
