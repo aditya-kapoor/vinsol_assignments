a21a8aa016c4b95b5c19137325397abeed183e3d
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-07-28 17:39:16 +0530

    JQuery Ex-9 Solution

---
+++ jquery-excercise-9/js/specials.js
@@ -0,0 +1,22 @@
+ $('document').ready(function(){
+   var json_data;
+  $specials = $('div#specials')
+  $form = $specials.find('form')
+  $select = $form.find('select')
+  $div = $('<div>testing text</div>')
+  $div.insertAfter($select)
+  $select.one('change',function(){
+     var x = $(this).val()
+     $.getJSON('data/specials.json', function(data){
+     json_data = data
+     str = json_data[x].title + "<br />" + json_data[x].text + "<br />" + json_data[x].image + "<br />" + json_data[x].color;
+     $div.html(" ")
+     $div.html(str)
+})
+   $select.change(function() {
+   str = json_data[$(this).val()].title + "<br />" + json_data[$(this).val()].text + "<br />" + json_data[$(this).val()].image + "<br />" + json_data[$(this).val()].color;
+     $div.html(" ")
+     $div.html(str)
+      }) 
+   })
+})
