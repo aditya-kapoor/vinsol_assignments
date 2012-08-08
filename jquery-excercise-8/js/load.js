b8f53deed92073f4384269cd24cee85d7d5110b4
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-08 19:36:40 +0530

    JQuery Excercise 8

--- jquery-excercise-8/js/load.js
+++ jquery-excercise-8/js/load.js
@@ -1,19 +1,19 @@
 $('document').ready(function(){
-  $blog = $('div#blog')
-  $ul = $blog.children().eq(1)
+  $ul = $('div#blog').children().eq(1)
   $ul_children = $ul.children()
   $ul_children.each(function(index){
   var $div = $('<div>Testing Text</div>')
   $div.insertAfter($(this).find('h3')) 
   $(this).find('h3').click(function(event) {
     event.preventDefault();
-    var data = $(this).find('a').attr('href');
-    $(this).find('a').data('href',data)
-    var url_data = $(this).find('a').data('href')
-    var url = "data/" + url_data
-    var parts = url.split("#")
-    var final_url = parts[0] + " #" + parts[1]
-    $(this).next('div').load(final_url);
+		$link = $(this).find('a')
+    var data = $link.attr('href');  
+    $link.data('href',data) 
+    var url_data = $link.data('href') 				 // extracting the href data out of the link
+    var url = "data/" + url_data      				 // Making the URL for the fetching of the data
+    var parts = url.split("#")        
+    var final_url = parts[0] + " #" + parts[1] // Adding a Space between link and #
+    $(this).next('div').load(final_url);       // Perform the final load operation
     //console.log($(this).next('div').text());
      })
    });
