0ba580fd26a9a42ab2db6bab24748a8b187aec3e
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-09 18:01:53 +0530

    JQuery Excercise-7

--- jquery-excercise-7/js/navigation.js
+++ jquery-excercise-7/js/navigation.js
@@ -1,9 +1,9 @@
 $('document').ready(function(){
   	$list = $('ul#nav')
   	$list.children().each(function(index){  
-	  	$list.children().eq(index).hover(
-	  		function(){ $(this).find('ul').addClass("hover") }, 
-			function(){ $(this).find('ul').removeClass("hover") }
+	  	$list.children().eq(index).hover( function() { 
+	  		$(this).find('ul').addClass("hover") },	function(){ 
+	  			$(this).find('ul').removeClass("hover") }
 			)  
 	 	})
  });
