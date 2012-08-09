5510f2b20496a4d051fc11b8c3b98b57defe98c1
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-09 19:27:36 +0530

    Jquery Ex-9 Solution

--- jquery-excercise-9/js/specials.js
+++ jquery-excercise-9/js/specials.js
@@ -1,23 +1,32 @@
  $('document').ready(function(){
-   var json_data;
-  $specials = $('div#specials')
-  $form = $specials.find('form')
-  $select = $form.find('select')
-  $div = $('<div>testing text</div>')
-  $div.insertAfter($select)
-  $select.one('change',function(){
-     var x = $(this).val()
-     $specials.find('li').eq(1).hide()
-     $.getJSON('data/specials.json', function(data){
-     json_data = data
-     str = json_data[x].title + "<br />" + json_data[x].text + "<br />" + json_data[x].image + "<br />" + json_data[x].color;
-     $div.html(" ")
-     $div.html(str)
-})
+    var json_data;
+    $specials = $('div#specials')
+    $form = $specials.find('form')
+    $select = $form.find('select')
+    $target_div = $('<div></div>')
+    $target_div.insertAfter($select)
+    $select.one('change',function(){
+       var select_value = $(this)
+       $specials.find('li').eq(1).remove()
+       $.getJSON('data/specials.json', function(data){
+          json_data = data
+          generateHTML(select_value)
+      })
+
+     function generateHTML(data){
+        $title = $('<h2></h2>')
+        $title.append(json_data[data.val()].title)
+        $img = $('<img />')
+        $img.attr("src",json_data[data.val()].image)
+        $text_para = $('<p></p>')
+        $text_para.append(json_data[data.val()].text)
+        $target_div.append($title).append($img).append($text_para)
+        $target_div.css("color",json_data[data.val()].color) 
+       }
+
    $select.change(function() {
-   str = json_data[$(this).val()].title + "<br />" + json_data[$(this).val()].text + "<br />" + json_data[$(this).val()].image + "<br />" + json_data[$(this).val()].color;
-     $div.html(" ")
-     $div.html(str)
+      $target_div.html(" ")
+      generateHTML($(this))
       }) 
    })
 })
