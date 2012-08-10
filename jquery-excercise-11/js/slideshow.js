6eb5e92e5f32efbd23a6bc7146eff4f7d136f19c
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-10 11:37:51 +0530

    Slideshow Solution

--- jquery-excercise-11/js/slideshow.js
+++ jquery-excercise-11/js/slideshow.js
@@ -2,30 +2,73 @@ $('document').ready(function () {
     $slideshow = $('#slideshow')
     $body = $('body')
     $body.prepend($slideshow)
-    $nav = $('<div id="navigation-slideshow"><table><tr><td>Current Slide</td><td><input type="text" id="nav-slide" readonly></td></tr><tr><td>Total Slide </td><td><input type="text" id="nav-slide-total" readonly /></td></tr></div>')
-    $nav.insertAfter($slideshow)
-    $slideshow.children().eq(0).addClass("active")
-    $slideshow.children().eq(0).siblings().attr("style", "display:none")
+    generateNavigationBox()
 
-    function GenerateSlideShow() {
+    function setTotalSlides(){
         $children = $slideshow.children()
         $('input#nav-slide-total').val($children.length)
+    }
+
+    function setCurrentSlide(active_image){
+        $index = (active_image.index() + 1) % 3
+        $('input#nav-slide').val($index)        
+    }
+
+    function GenerateSlideShow() {
         $active_image = $slideshow.find(".active")
-        console.log($active_image.index())
-        $index = ($active_image.index() + 1) % 3
-        $('input#nav-slide').val($index)
-        $active_image.fadeIn(1000).show(1500).fadeOut(1000, function () {
-            if ($active_image.next().length == 0) $children.eq(0).attr("style", "display:block")
-            else $active_image.next().attr("style", "display:block")
+        setCurrentSlide($active_image)
+        setTotalSlides()
+        $active_image.show(1000).fadeOut(1000, function () {
+            if ($active_image.next().length == 0) {
+                $children.eq(0).addClass("active")
+                $children.eq(0).fadeIn(1000)
+            } else { 
+                $active_image.next().addClass("active")
+                $active_image.next().fadeIn(1000)
+            }
         })
-        if ($active_image.next().length == 0) $children.eq(0).addClass("active")
-        else $active_image.next().addClass("active")
         $active_image.removeClass("active")
     }
 
+    function generateNavigationBox(){
+
+        $nav = $('<div></div>')
+        $nav.attr("id","navigation-slideshow")
+        $table = $('<table></table>')
+
+        $tr = $('<tr></tr>')
+        $currentSlide_td = $('<td></td>')
+        $currentSlide_td.append("Current Slide")
+        
+        $input_td = $('<td></td>')
+        $nav_slide = $('<input>')
+        $nav_slide.attr({type:"text",id:"nav-slide",readonly:"readonly"})
+        $input_td.append($nav_slide)
+        
+        $tr.append($currentSlide_td).append($input_td)
+
+        $next_tr = $('<tr></tr>')
+        $totalSlide_td = $('<td></td>')
+        $totalSlide_td.append("Total Slides")
+        
+        $total_input_td = $('<td></td>')
+        
+        $total_slide = $('<input>')
+        $total_slide.attr({type:"text",id:"nav-slide-total",readonly:"readonly"})
+        
+        $total_input_td.append($total_slide)
+        $next_tr.append($totalSlide_td).append($total_input_td)
+
+        $nav.append($tr).append($next_tr)
+
+        $nav.insertAfter($slideshow)
+        $slideshow.children().eq(0).addClass("active")
+        $slideshow.children().eq(0).siblings().hide()
+    }
+    
     $(function () {
         setInterval(function () {
             GenerateSlideShow()
-        }, 4000)
+        }, 3500)
     })
 })
