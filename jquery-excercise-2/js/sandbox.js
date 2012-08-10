0b92207d6f4c1b765f3102781263aa69c5d8289c
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-10 16:26:36 +0530

    JQuery Traversing Excercise

--- jquery-excercise-2/js/sandbox.js
+++ jquery-excercise-2/js/sandbox.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
    /*"Excercise 2.2.1"*/
    /*"Select all of the image elements on the page; log each image's alt attribute."*/
    $('img').each(function(index,element){ 
-   console.log($(element).attr("alt")) 
+   		console.log($(element).attr("alt")) 
    });
 
    /*"Excercise 2.2.2"*/
@@ -12,15 +12,13 @@ $(document).ready(function(){
 
    /*"Excercise 2.2.3"*/
    /*"Select the list item inside #myList that has a class of 'current' and remove that class from it; add a class of 'current' to the next list item."*/
-   var selectedItem = $('#myList .current')
-   selectedItem.next().addClass("current")
-   selectedItem.remove()
+   $('#myList .current').removeClass("current").next().addClass("current")
    
    /*"Excercise 2.2.4"*/
    /*"Select the select element inside #specials; traverse your way to the submit button"*/
-   start = $('#specials form select')
-   end = $('#specials form .buttons')
    
+	$('#specials select').parent().siblings().find('input[type="submit"]')
+
   /*"Excercise 2.2.5"*/
   /*"Select the first list item in the #slideshow element; add the class 'current' to it, and then add a class of 'disabled' to its sibling elements."*/
   $('#slideshow :first-child').eq(0).addClass("current")
