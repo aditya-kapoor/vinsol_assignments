72d49908c911895b91c286d9e1e2b0059302f69c
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-13 21:59:40 +0530

    Fibonacci Series

--- generateFibonacci.rb
+++ generateFibonacci.rb
@@ -4,9 +4,8 @@ def generateFibonacci(num=1000)
 	series_number = 0
 	while (series_number < num) do
 		series_number = initial_number1 + initial_number2
-		initial_number1 = initial_number2
-		initial_number2 = series_number
-		puts "#{series_number}"
+		initial_number1, initial_number2 = initial_number2, series_number
+		print "#{series_number} "
 	end
 end
 
