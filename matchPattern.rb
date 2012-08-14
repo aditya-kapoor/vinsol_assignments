6a24da46f0b6a1e05ec1230792459b80a17c166f
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-14 13:47:49 +0530

    Pattern Matching

---
+++ matchPattern.rb
@@ -0,0 +1,15 @@
+def matchPattern(str,pattern)
+	occurence_count = 0
+	pattern = Regexp.new(pattern,true)
+	new_str = str.gsub(pattern) do |match|  
+				occurence_count+=1
+				"(#{match})" 
+				end 
+	puts new_str
+	puts "Total Occurences found: #{occurence_count}"
+end
+puts "Enter An Input String : "
+input_string = gets.chomp
+puts "Enter A Pattern : "
+pattern = gets.chomp
+matchPattern(input_string,pattern)
