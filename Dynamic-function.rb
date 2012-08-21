47838a9552d60b4866f5a4dd7f0cb64b3339b041
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-21 17:59:15 +0530

    Dynamic Function

--- Dynamic-function.rb
+++ Dynamic-function.rb
@@ -1,10 +1,10 @@
 class Function
-	def self.create_dynamic_function(name,lines_of_code)
-		define_method(name) do |*args|
-			instance_eval(lines_of_code)
-		end
-	end
-end 
+  def self.create_dynamic_function(name,lines_of_code)
+    define_method(name) do |*args|
+      instance_eval(lines_of_code)
+       end
+    end
+end
 object = Function.new
 puts "Enter the name of the function"
 name = gets.chomp
