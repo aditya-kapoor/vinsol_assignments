6b5768f63a8d435bc1f30143b41e5dcf282d9524
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-21 13:38:14 +0530

    Palindrome

--- palindrome.rb
+++ palindrome.rb
@@ -1,17 +1,15 @@
 def get_palindrome
-
 	choice = 'y'
-	while choice == 'y'
+	while !choice.match(/q/i)
 		puts "Enter an Input String"
-		str = gets
-		str = str.chomp
+		str = gets.chomp
 		str.downcase!
 		if(str == str.reverse)
 			puts "The String is a palindrome"
 		else
 			puts "The String is not a palindrome"
 		end
-		puts "Do You want to test more strings (y|n)"
+		puts "Do You want to quit (q|Q)"
 		choice = gets
 		choice = choice.chomp
 	end
