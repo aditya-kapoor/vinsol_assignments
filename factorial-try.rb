8c8dcaeb6d4b3db8e08d359d1179d4697ffd24db
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-21 18:43:06 +0530

    Factorial Try

--- factorial-try.rb
+++ factorial-try.rb
@@ -1,17 +1,17 @@
 def factorial(n)
-	begin 
-		if n < 0
-			raise
-		else
-			prod = 1
-			for i in (1..n)
-			prod *= i
-		end
-	prod
-	end
-	rescue
-		print "Please Enter a Positive Number"
-	end
+  begin 
+    if n < 0
+      raise
+    else
+      prod = 1
+      for i in (1..n)
+        prod *= i
+      end
+  prod
+  end
+  rescue
+    print "Please Enter a Positive Number"
+  end
 end
 
 puts "Enter a Number : "
