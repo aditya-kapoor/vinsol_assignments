76c86e2c6d36b9d5da2c355a98764d2343b9053b
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-21 18:40:30 +0530

    Factorial Using Ranges

--- factorial-ranges.rb
+++ factorial-ranges.rb
@@ -1,9 +1,9 @@
 def factorial(n)
-	prod = 1
-	for i in (1..n)
-		prod *= i
-	end
-	"Fatorial Is: #{prod}" 
+  prod = 1
+  for i in (1..n)
+    prod *= i
+  end
+  "Fatorial Is: #{prod}" 
 end
 
 puts "Enter a Number : "
