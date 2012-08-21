a3efbcac354659dcb4700f8828f4d73190c5407c
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-21 17:56:04 +0530

    Dynamic Function

--- Dynamic-function.rb
+++ Dynamic-function.rb
@@ -5,11 +5,11 @@ class Function
 		end
 	end
 end 
-f = Function.new
+object = Function.new
 puts "Enter the name of the function"
 name = gets.chomp
 puts "Enter the line of code"
 lines_of_code = gets.chomp
 Function.create_dynamic_function(name,lines_of_code)
 puts "Calling the Dynamically created function"
-puts f.send(name)
+puts object.send(name)
