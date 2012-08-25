4a4594506004981f63b9649577339a5fb6853f1d
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-25 14:15:47 +0530

    Occurences

--- Basic-Ruby/Occurences.rb
+++ Basic-Ruby/Occurences.rb
@@ -1,14 +1,15 @@
 def getHash()
-puts "Enter an Input String"
-answer = gets.chomp
-hash = Hash.new(0)
-pattern = /[A-Z]/i
-answer.each_char do |n|
-if(n.match(pattern))
+  puts "Enter an Input String"
+  answer = gets.chomp
+  hash = Hash.new(0)
+  pattern = /[A-Z]/i
+  answer.each_char do |n|
+  if(n.match(pattern))
     hash[n] += 1
   end
 end  
-hash
+  hash
 end
+
 hash = getHash()
 puts hash
