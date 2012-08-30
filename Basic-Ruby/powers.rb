81b61a30f38b95f8d8d4fbc88ead4332e617e913
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-30 17:31:59 +0530

    Power with inject

--- Basic-Ruby/powers.rb
+++ Basic-Ruby/powers.rb
@@ -1,9 +1,15 @@
 class Array
+  @powers 
   def powers(powers)
     output_array = []
     self.inject(powers) { |a,b| output_array.push(b**powers) }
     output_array
   end
+  def powers2(powers)
+    @powers = powers
+    self.collect { |i| i**@powers }
+  end
 end
 
 puts [1,2,3,4,5,6,7].powers(3)
+puts [1,2,3,4,5,6,7].powers2(3)
