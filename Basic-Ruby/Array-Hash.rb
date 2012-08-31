daf3bcb18475e4c3ac0abec623caf44655dafeda
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-31 13:26:54 +0530

    Array Hash With Fetch

--- Basic-Ruby/Array-Hash.rb
+++ Basic-Ruby/Array-Hash.rb
@@ -1,18 +1,14 @@
 def calculate_hash(arr)
   hash = Hash.new(0) 
-  for i in 0...arr.length
-    len = arr[i].to_s.length
-    if(hash.has_key?(len)) 
-      hash[len] << arr[i]
-    else
-      hash[len] = Array.new()
-      hash[len] << arr[i]
-    end
+  arr.each do |element|
+    len = element.to_s.length
+    hash[len] = hash.fetch(len, [])
+    hash[len].push(element)
   end
   hash
 end
 
 if(__FILE__ == $0)
   arr = ['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ']
-  calculate_hash(arr)
+  puts calculate_hash(arr)
 end
