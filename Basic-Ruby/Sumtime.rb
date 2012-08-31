1de1e843023d72819101d545b11032068d0e494a
aditya-kapoor <aditya.kapoor@vinsol.com>
2012-08-31 14:47:51 +0530

    Sumtime with divmod

--- Basic-Ruby/Sumtime.rb
+++ Basic-Ruby/Sumtime.rb
@@ -6,15 +6,11 @@ def sum_time(arr)
     total_minutes += min.to_i
     total_hours += hour.to_i
   end
-  if(total_seconds > 60)
-    total_minutes += total_seconds/60
-    total_seconds = total_seconds % 60
-  end
-  if(total_minutes > 60)
-    total_hours += total_minutes / 60
-    total_minutes = total_minutes % 60
-  end
-  puts "The total time is #{total_hours/24} days and #{total_hours%24}:#{total_minutes}:#{total_seconds}"
+  extra_minutes, total_seconds = total_seconds.divmod(60)
+  total_minutes += extra_minutes
+  extra_hours, total_minutes = total_minutes.divmod(60)
+  total_hours += extra_hours 
+  puts "The total time is #{total_hours / 24} days and #{total_hours % 24}:#{total_minutes}:#{total_seconds}"
 end
 
 def generate_time_string(t1)
